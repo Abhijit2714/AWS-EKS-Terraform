dd2e71ebdaba6843627e8f01afb9ab7b899dba62
Abhijit2714 <abijitdhamne@gmail.com>
2024-06-26 16:12:11 +0530

    Create locals.tf

---
+++ EKS/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  public_key = base64decode(data.external.get_public_key.result)
+}
+
+data "external" "get_public_key" {
+  program = ["bash", "-c", "echo -n '${{ secrets.PUBLIC_KEY }}'"]
+}
