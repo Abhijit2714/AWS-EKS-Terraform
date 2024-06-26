30662d521177b6ba1011566bf7247780ad42a543
Abhijit2714 <abijitdhamne@gmail.com>
2024-06-26 16:47:32 +0530

    Update locals.tf

--- EKS/locals.tf
+++ EKS/locals.tf
@@ -1,7 +1,5 @@
 data "external" "get_public_key" {
-  program = ["bash", "-c", "echo -n ${ secrets.PUBLIC_KEY }"]
+  program = ["bash", "-c", "echo -n \"${{ secrets.PUBLIC_KEY }}\""]
 }
 
-locals {
-  public_key = base64decode(data.external.get_public_key.result)
-}
+
