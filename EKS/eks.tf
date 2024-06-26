8ce0d3c3ba4e0bc9ec7019a9805877dc2fde2f79
Abhijit2714 <abijitdhamne@gmail.com>
2024-06-26 17:24:15 +0530

    Update eks.tf

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -82,7 +82,7 @@ resource "aws_launch_template" "eks-with-disks" {
 
 resource "aws_key_pair" "ssh-key-ec2-instance" {
   key_name   = var.ssh-keyname
-  public_key = var.TF_VAR_public_key
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDLnxDpm0xmNPYikUY0NYDP+/dWnAgKXt6UvVZz4D6Rc6qyfQr5ZzBRNE9XPDyN+H9CO6BucxxOo2FCVVh4iryULcfkhyXRAetURnSVtEx555KCFBL8oveQE98u6tScHafH6ew4Fcmsw4j2t5YRL3li5tfrdFwNaGYW+E/zhGycU/tEACP30PkGxgNKPeEBa5ehzKnn2iBguVfJkj4YF87scRCI6kKWuveacxvoSt/51MxGM+NVifOYhgWSUfe43b2xvp+t2JE90v1a7/Wrpf96dS9EYgjp4GnWtSv0L8r3idT9DDVkCZbPH21h8jyVd277VGeZXPR4KTNbc6Hg0MH5JgZVegMFa37/GuzEbWH1PRUtyWFbt/CnFLZ39b/htdgqCRBR4eTdbR5K5Ojgc61uiKUlrlDkPeLn8yFzJDzNSQKLmxNbsw9pt6xXahXiINZX6TfLjGP/zoyNN3pOesaWFQFCqpdUfTNOkNOaVrU/dYvXETysA81XKPj8psjgTKjP0yd5tshnbSRIR3Ie6FlTogTePf6+xRKmZYQQjBHqtnxVCIbc1bMjpRb4wS5JWNdB70vGDhNexo8pigfgOu28Dln7/jFsm+V7gle4zx4NZbB9UDqfvEgCvcovfcP9nSlV/cRfI1l9mwtZCaLY/IN2U4hv8S8rIt0RlOj31M3CIw== abijitdhamne@gmail.com"
 }
 
 data "aws_ami" "ubuntu" {
